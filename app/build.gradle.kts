54ec3f731c8ad4a02c178fddd354fe62d01bf25f
Yashraj Paralikar <p.yashraj@gmail.com>
2025-08-31 14:32:08 +0530

    - Updated App version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 2
-        versionName = "0.1.1"
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
